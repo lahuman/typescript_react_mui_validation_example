7b4050a7a515c52374ddfbfdfd438994010ba8c2
Daniel Lim <lahuman@daum.net>
2022-10-25 14:14:03 +0900

    feat : update example using max / min Length

--- src/Components/AddressModel.ts
+++ src/Components/AddressModel.ts
@@ -1,4 +1,10 @@
-import { BaseModel, RegexAndMsg, NumberAndMsg } from "react-mui-validation";
+import {
+  BaseModel,
+  RegexAndMsg,
+  NumberAndMsg,
+  KeyAndKeyAndMsg,
+  KeyAndNumber,
+} from "react-mui-validation";
 
 export default class AddressModel extends BaseModel {
   //  static required = ["firstName", "lastName", 'email', 'address1', 'address2', 'city', 'zip', 'country'];
@@ -10,15 +16,35 @@ export default class AddressModel extends BaseModel {
     city: "도시명은 필수 값입니다.",
     zip: "우편번호는 필수 값입니다.",
     country: "국가는 필수 값입니다.",
-    dtm: "예약일자를 선택하세요!"
+    dtm: "예약일자를 선택하세요!",
   };
 
+  static minLength = {
+    firstName: {
+      num: 3,
+      msg: "최소 3자 이상 작성해주세요.",
+    },
+    lastName: {
+      num: 3,
+      msg: "최소 3자 이상 작성해주세요.",
+    },
+  };
+  static maxLength = {
+    firstName: {
+      num: 10,
+      msg: "최대  10자까지 작성해주세요.",
+    },
+    lastName: {
+      num: 10,
+      msg: "최대  10자까지 작성해주세요.",
+    },
+  };
   // static min = { orderCnt: 3 };
   static min = { orderCnt: new NumberAndMsg(3, "최소 3이상을 가져야합니다.") };
 
   // static max = { orderCnt: 30 };
   static max = {
-    orderCnt: new NumberAndMsg(10, "최대 10이하를 가져야 합니다.")
+    orderCnt: new NumberAndMsg(10, "최대 10이하를 가져야 합니다."),
   };
 
   // static same = { email: ["email2"] };
@@ -33,7 +59,7 @@ export default class AddressModel extends BaseModel {
     ),
     dtm: new RegexAndMsg(
       /\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01]) (0[0-9]|1[0-9]|2[0-4]):(0[0-9]|[1-5][0-9])/,
-      '예약일자 날짜 형식을 확인하세요.',
+      "예약일자 날짜 형식을 확인하세요."
     ),
   };
 
@@ -65,7 +91,7 @@ export default class AddressModel extends BaseModel {
       zip,
       country,
       orderCnt,
-      dtm
+      dtm,
     } = data;
 
     this.firstName = firstName ?? "";
